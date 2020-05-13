dfd6b45d876bea1eef08be22244f619dab2f0f78
mohit138 <39694943+mohit138@users.noreply.github.com>
2020-05-13 15:24:27 +0530

    prediction and optimal polynomial correction.

--- logistic_iris/data_analysis.py
+++ logistic_iris/data_analysis.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from pylab import rcParams
+rcParams['figure.figsize'] = 15, 9
+
 op_cases= 3 
 #iris_df = pd.read_csv("Iris.csv",index_col='Id')
 train_df = pd.read_csv("TrainData.csv")
@@ -30,7 +33,7 @@ def costFunction(X,y,theta,lam):
     prod=X.dot(theta)
     h=1/(1+np.exp(-prod))
     
-    J = (-1/m)*np.sum( y*np.log(h) + (1-y)*np.log(1-h)) + (lam/(2*m))*((np.sum(theta))-np.sum(theta[0,:]));
+    J = (-1/m)*np.sum( y*np.log(h + 1e-20) + (1-y)*np.log(1-h + 1e-20)) + (lam/(2*m))*((np.sum(theta))-np.sum(theta[0,:]));
     return [J,h]
 
 
@@ -40,7 +43,10 @@ def grad_descent_vec(X,y,theta,lam,max_itr,alpha):
     for i in range(max_itr):
         [J,h]=costFunction(X,y,theta,lam)
         dif = h-y
-        theta = theta - (alpha/m)*((X.T).dot(dif))
+
+        tho = lam/m*theta[:,0:1]
+        theta = theta - (alpha/m)*((X.T).dot(dif) + lam/m*theta) 
+        theta[:,0:1] = theta[:,0:1] - tho
         J_hist[i]=J
     return [theta,J_hist]
 
@@ -52,13 +58,53 @@ def learning_curve(xT,xV,yT,yV,theta,lam):
         [J_t_error[i-2],h] = costFunction(xT[0:i-1,:], yT[0:i-1,:], theta, lam)
         [J_v_error[i-2],h] = costFunction(xV[0:i-1,:], yV[0:i-1,:], theta, lam)
     i=np.arange(1,ma,1)
-    print(J_t_error,J_v_error)
+    #print(J_t_error,J_v_error)
     plt.plot(i,J_t_error)
     plt.plot(i,J_v_error)
     plt.show()
 
 
+# the function to find degree for best results. 
+def lc_degree(d_max,xT,xV,yT,yV,lam,max_itr,alpha,col):
+    power=1
+    alpha =.01
+    max_itr = 1000
+    ma=yV.shape[0]
+    J_t_error = np.zeros((d_max,1))
+    J_v_error = np.zeros((d_max,1))
+    for i in range(d_max):
+        initial_theta = np.ones((xT.shape[1],yT.shape[1]))
+        [theta,j_hist] = grad_descent_vec(xT, yT, initial_theta,lam, max_itr, alpha)    
+        [J_t_error[i],h] = costFunction(xT[0:ma,:], yT[0:ma,:], theta, lam)
+        [J_v_error[i],h] = costFunction(xV, yV, theta, lam)
+        fact = normalise_fact( np.append(np.power(xT,i+1) , np.power(xV,i+1),axis=0) )
+        #fact=1
+        powT = (np.power(xT,i+1))/fact
+        powV = (np.power(xV,i+1))/fact
+        xT = np.append(xT, powT,axis=1)
+        xV = np.append(xV, powV,axis=1)
+        j=np.arange(1,1001,1)
+        #plt.plot(j,j_hist)
+        #plt.show()
+        #xT = np.power(xT,i+1) / fact
+        #xV = np.power(xV,i+1) / fact
+        #print(j_hist[980:1000])
+    power = np.argmin(J_v_error)
+    i=np.arange(1,d_max+1,1)
+    plt.plot(i,J_t_error,label='Train Error')
+    plt.plot(i,J_v_error,label='Validation Error')
+    plt.legend(loc='upper left')
+    plt.title(col)
+    plt.show()
+    return power
 
+def normalise_fact(x):
+    Max = np.max(x)
+    Min = np.min(x)
+    fact = (Max-Min)
+    return fact
+
+'''
 # the function to find degree for best results. 
 def lc_degree(d_max,xT,xV,yT,yV,lam,max_itr,alpha):
     
@@ -82,7 +128,7 @@ def lc_degree(d_max,xT,xV,yT,yV,lam,max_itr,alpha):
     plt.plot(i,J_v_error,label='Validation Error')
     plt.legend(loc='upper left')
     plt.show()
-    
+'''
 
 def lc_lambda(limit,xT,xV,yT,yV,lam,max_itr,alpha):
     ma=yV.shape[0]
@@ -105,6 +151,27 @@ def normalise(x):
     Min = np.min(x)
     x = x / (Max-Min)
     return x
+
+def prediction(h,y):
+    
+    count =0
+    h_out = np.zeros(h.shape)
+    
+    for i in range(h.shape[0]):
+        if h.shape[1] == 1:
+            if h[i] >= 0.5:
+                h_out[i] = 1
+        else:
+            h_out[i, np.argmax(h[i,:])] =1
+    
+    
+    for i in range(h.shape[0]):
+        for j in range(h.shape[1]):
+            if h_out[i,j]!=y[i,j]:
+                count = count+1
+                break
+    acc = (h.shape[0] - count)/h.shape[0] *100
+    return [h_out, acc]
 
 '''
 def string_to_num(x):
@@ -157,25 +224,55 @@ lam=0
 #x_val[:,1:2] = normalise(np.power(x_val[:,1:2],3))
 #x_test[:,1:2] = normalise(np.power(x_test[:,1:2],3))
 
-[theta,j_hist] = grad_descent_vec(x_train, y_train, initial_theta,0, max_itr, alpha)
+power = [5,8,5,8]
+# Adding polynomial terms to the first 8 features 
+for i in range(1,5):
+     if power[i-1] != 0:
+         for j in range(2,power[i-1]+1):
+             #print(j);
+             fact = normalise_fact( np.append( np.append( np.power(x_train[:,i:i+1],j) , np.power(x_val[:,i:i+1],j), axis=0) , np.power(x_test[:,i:i+1],j) ,axis=0) )
+             #fact=1
+             #print(np.append( np.append( np.power(x_train[:,i:i+1],j) , np.power(x_val[:,i:i+1],j), axis=0) , np.power(x_test[:,i:i+1],j) ,axis=0).shape)
+             x_train = np.append(x_train, (np.power(x_train[:,i:i+1],j) / fact),axis=1)
+             x_val = np.append(x_val, (np.power(x_val[:,i:i+1],j) / fact),axis=1)
+             x_test = np.append(x_test, (np.power(x_test[:,i:i+1],j) / fact),axis=1)
+
+
+initial_theta=np.ones((x_train.shape[1],y_train.shape[1]))
+[theta,j_hist] = grad_descent_vec(x_train, y_train, initial_theta,lam, max_itr, alpha)
 
 i=np.arange(1,151,1)
 
+learning_curve(x_train, x_val, y_train, y_val, theta, 0)
 
-learning_curve(x_train, x_val, y_train, y_val, theta, lam)
 
+col = ["sl","sw","pl","pw"]
 # find the optimal degree for each function for sepal length feature.
 d_max=13
 
 #print(x_train)
 #for i in range(4):
-#    lc_degree(d_max, x_train[:,i+1:i+2], x_val[:,i+1:i+2], y_train, y_val, lam, max_itr, alpha)
+#    lc_degree(d_max, x_train[:,i+1:i+2], x_val[:,i+1:i+2], y_train, y_val, lam, max_itr, alpha,col[i])
 
 
 limit = 11
 # obtain optimal lambda
 lc_lambda(limit, x_train, x_val, y_train, y_val, lam, max_itr, alpha)
 
+
+[J,h_train]=costFunction(x_train, y_train, theta,0)
+
+h_train,acc=prediction(h_train,y_train)
+print("\naccuracy (train):  ",acc)
+
+[J,h_test]=costFunction(x_test, y_test, theta,0)
+
+h_test,acc=prediction(h_test,y_test)
+print("\naccuracy:  ",acc)
+
+
+for i in range(y_test.shape[0]):
+  print( "{}      {} " .format(h_test[i] , y_test[i]))
 
 # THEREFORE AFTER THE ANALYSIS, WE FOUND
 # WE NEED TO USE, DEGREE 3 FOR SEPAL LENGTH AND SEPAL WIDTH.
