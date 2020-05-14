883ae21b7828ea0e0bd3054431cf699a3081e0a4
mohit138 <39694943+mohit138@users.noreply.github.com>
2020-05-14 19:41:34 +0530

    Neural Network implementation
    Single Hidden Layer Neural Network is implemented, and irir data set is used.

---
+++ neural_iris/data_analysis.py
@@ -0,0 +1,337 @@
+# data analysis for IRIS
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from pylab import rcParams
+from scipy.special import expit
+rcParams['figure.figsize'] = 15, 9
+
+op_cases= 3 
+#iris_df = pd.read_csv("Iris.csv",index_col='Id')
+train_df = pd.read_csv("TrainData.csv")
+data_t = train_df.to_numpy()
+x_train = data_t[:,1:5]
+y_train = data_t[:,5:8]
+
+val_df = pd.read_csv("ValData.csv")
+data_v = val_df.to_numpy()
+x_val = data_v[:,1:5]
+y_val = data_v[:,5:8]
+
+test_df = pd.read_csv("TestData.csv")
+data_te = test_df.to_numpy()
+x_test = data_te[:,1:5]
+y_test = data_te[:,5:8]
+
+#pd.plotting.scatter_matrix(iris_df)
+#plt.show()
+
+def costFunction(X,y,theta,lam):
+    m=X.shape[0]
+    prod=np.zeros((X.shape[0],theta.shape[1]))
+    
+    prod=X.dot(theta)
+    h=1/(1+np.exp(-prod))
+    
+    J = (-1/m)*np.sum( y*np.log(h + 1e-20) + (1-y)*np.log(1-h + 1e-20)) + (lam/(2*m))*((np.sum(theta))-np.sum(theta[0,:]));
+    return [J,h]
+
+
+def grad_descent_vec(X,y,theta,lam,max_itr,alpha):
+    m=X.shape[0]
+    J_hist=np.zeros((max_itr,1))
+    for i in range(max_itr):
+        [J,h]=costFunction(X,y,theta,lam)
+        dif = h-y
+
+        tho = lam/m*theta[:,0:1]
+        theta = theta - (alpha/m)*((X.T).dot(dif) + lam/m*theta) 
+        theta[:,0:1] = theta[:,0:1] - tho
+        J_hist[i]=J
+    return [theta,J_hist]
+
+
+
+
+# the function to find degree for best results. 
+def lc_degree(d_max,xT,xV,yT,yV,lam,max_itr,alpha,col):
+    power=1
+    alpha =.01
+    max_itr = 1000
+    ma=yV.shape[0]
+    J_t_error = np.zeros((d_max,1))
+    J_v_error = np.zeros((d_max,1))
+    for i in range(d_max):
+        initial_theta = np.ones((xT.shape[1],yT.shape[1]))
+        [theta,j_hist] = grad_descent_vec(xT, yT, initial_theta,lam, max_itr, alpha)    
+        [J_t_error[i],h] = costFunction(xT[0:ma,:], yT[0:ma,:], theta, lam)
+        [J_v_error[i],h] = costFunction(xV, yV, theta, lam)
+        fact = normalise_fact( np.append(np.power(xT,i+1) , np.power(xV,i+1),axis=0) )
+        #fact=1
+        powT = (np.power(xT,i+1))/fact
+        powV = (np.power(xV,i+1))/fact
+        xT = np.append(xT, powT,axis=1)
+        xV = np.append(xV, powV,axis=1)
+        j=np.arange(1,1001,1)
+        #plt.plot(j,j_hist)
+        #plt.show()
+        #xT = np.power(xT,i+1) / fact
+        #xV = np.power(xV,i+1) / fact
+        #print(j_hist[980:1000])
+    power = np.argmin(J_v_error)
+    i=np.arange(1,d_max+1,1)
+    plt.plot(i,J_t_error,label='Train Error')
+    plt.plot(i,J_v_error,label='Validation Error')
+    plt.legend(loc='upper left')
+    plt.title(col)
+    plt.show()
+    return power
+
+def normalise_fact(x):
+    Max = np.max(x)
+    Min = np.min(x)
+    fact = (Max-Min)
+    return fact
+
+
+def lc_lambda(limit,xT,xV,yT,yV,lam,max_itr,alpha):
+    ma=yV.shape[0]
+    J_t_error = np.zeros((limit,1))
+    J_v_error = np.zeros((limit,1))
+    for i in range(limit):
+        lam = .01*pow(2,i)
+        initial_theta = np.ones((xT.shape[1],yT.shape[1]))
+        [theta,j_hist] = grad_descent_vec(xT, yT, initial_theta,lam, max_itr, alpha)    
+        [J_t_error[i],h] = costFunction(xT[0:ma,:], yT[0:ma,:], theta, lam)
+        [J_v_error[i],h] = costFunction(xV, yV, theta, lam)
+    i=np.arange(1,limit+1,1)
+    plt.plot(i,J_t_error,label='Train Error')
+    plt.plot(i,J_v_error,label='Validation Error')
+    plt.legend(loc='upper left')
+    plt.show()
+
+def learning_curve(hlSize,numLabel,xT,xV,yT,yV,theta,lam=0):
+    ma=yV.shape[0]
+    J_t_error = np.zeros((ma-1,1))
+    J_v_error = np.zeros((ma-1,1))
+    for i in range(2,ma+1):
+        [J_t_error[i-2],grad,h] = cost_function_NN1(theta,hlSize,numLabel,xT[0:i-1,:], yT[0:i-1,:], lam)
+        [J_v_error[i-2],grad,h] = cost_function_NN1(theta,hlSize,numLabel,xV[0:i-1,:], yV[0:i-1,:],  lam)
+    i=np.arange(1,ma,1)
+    #print(J_t_error,J_v_error)
+    plt.plot(i,J_t_error,label='Train Error')
+    plt.plot(i,J_v_error,label='Validation Error')
+    plt.legend(loc='upper left')
+    plt.show()
+    
+    
+def sigmoidGrad(z):
+    return expit(z)*(1-expit(z))
+
+# cost function for 1 hidden layer Neural Network
+def cost_function_NN1(theta,hlSize,numLabel,X,y,lam=0):
+    
+    
+    m= X.shape[0]
+    ipSize = X.shape[1]
+    J=0
+
+    t1 = theta[0:hlSize*(ipSize +1)].reshape(hlSize,ipSize+1)
+    t2 = theta[hlSize*(ipSize +1) : (hlSize*(ipSize +1)) + (numLabel*(hlSize +1)) +1].reshape(numLabel,hlSize+1)
+
+    tGrad1 = np.zeros(t1.shape)
+    tGrad2 = np.zeros(t2.shape)
+    #print(X.shape,t1.shape)
+    # Forward propogation algorithm
+    z2 = np.append(np.ones((m,1)),X,axis =1).dot(t1.T)
+    a2 = expit(z2)
+    z3 = np.append(np.ones((m,1)),a2,axis=1).dot(t2.T)
+    a3 = expit(z3)
+    h = a3
+    
+    
+    J = (-1/m)*np.sum( y*np.log(h + 1e-20) + (1-y)*np.log(1-h + 1e-20)) 
+    #Reg = 
+    
+    # Back propogation algorithm
+    D2=np.zeros(t2.shape)
+    D1=np.zeros(t1.shape)
+    err3 = h - y
+    l = t2.shape[1]
+    err2 = (err3.dot(t2[:,1:l])) * sigmoidGrad(z2) 
+    for i in range(m):
+        D2 = D2 + (err3[i:i+1,:].T).dot(np.append(np.ones((1,1)),a2[i:i+1,:],axis=1))
+        D1 = D1 + (err2[i:i+1,:].T).dot(np.append(np.ones((1,1)),X[i:i+1,:],axis=1))
+    
+    tGrad1 = D1/m
+    #tGrad1[:,1:tGrad1.shape[1]] = tGrad1[:,1:tGrad1.shape[1]] + lam/m*t1[:,1:t1.shape[1]]
+    tGrad2 = D2/m
+    #tGrad2[:,1:tGrad2.shape[1]] = tGrad2[:,1:tGrad2.shape[1]] + lam/m*t2[:,1:t2.shape[1]]
+
+    #print(tGrad1, tGrad2)
+    grad = np.concatenate([tGrad1.flat, tGrad2.flat])
+    #print(grad.shape)
+    return [J,grad,h]
+
+def gradient_descent_NN(X,y,theta,lam,max_itr,alpha,hlSize): # without regularisation !
+    m=X.shape[0]
+    J_hist=np.zeros((max_itr,1))
+    
+    numLabel = y.shape[1]
+    for i in range(max_itr):
+        [J,grad,h]=cost_function_NN1(theta, hlSize, numLabel, X, y, lam)
+        
+        theta = theta - (alpha/m)*(grad) 
+        J_hist[i]=J
+    return [theta,J_hist]
+
+def gradient_check(theta,hlSize,numLabel,X,y):
+    #m= X.shape[0]
+    #ipSize = X.shape[1]
+    appGrad = np.zeros(theta.shape)
+
+    #t1 = theta[0:hlSize*(ipSize +1)].reshape(hlSize,ipSize+1)
+    #t2 = theta[hlSize*(ipSize +1) : (hlSize*(ipSize +1)) + (numLabel*(hlSize +1))+1].reshape(numLabel,hlSize+1)
+
+    #tGrad1 = np.zeros(t1.shape)
+    #tGrad2 = np.zeros(t2.shape)
+    epsilon =.0001
+    
+    for i in range(theta.shape[0]):
+        posTheta = theta
+        posTheta[i:i+1] = posTheta[i:i+1] + epsilon
+        posJ,gr,h = cost_function_NN1(posTheta, hlSize, numLabel, X, y)
+        negTheta = theta
+        negTheta[i:i+1] = negTheta[i:i+1] - epsilon
+        negJ,gr,h = cost_function_NN1(negTheta, hlSize, numLabel, X, y)
+        appGrad[i]=((posJ-negJ)/(2*epsilon))
+    return appGrad
+    '''
+    for i in range(t1.shape[0]):
+        
+        for j in range(t1.shape[1]):
+            posTheta = t1
+            posTheta[i:i+1,j:j+1] = posTheta[i:i+1,j:j+1] + epsilon
+            
+            # Forward propogation algorithm for pos theta
+            z2 = np.append(np.ones((m,1)),X,axis=1).dot(posTheta.T)
+            a2 = expit(z2)
+            z3 = np.append(np.ones((m,1)),a2,axis=1).dot(t2.T)
+            a3 = expit(z3)
+            h = a3
+            posJ = (-1/m)*np.sum( y*np.log(h + 1e-20) + (1-y)*np.log(1-h + 1e-20)) 
+            
+            
+            negTheta = t1
+            negTheta[i:i+1,j:j+1] = negTheta[i:i+1,j:j+1] - epsilon
+           
+            # Forward propogation algorithm for neg theta
+            z2 = np.append(np.ones((m,1)),X,axis=1).dot(negTheta.T)
+            a2 = expit(z2)
+            z3 = np.append(np.ones((m,1)),a2,axis=1).dot(t2.T)
+            a3 = expit(z3)
+            h = a3
+            negJ = (-1/m)*np.sum( y*np.log(h + 1e-20) + (1-y)*np.log(1-h + 1e-20)) 
+            
+            tGrad1[i,j] = (posJ - negJ) / (2 * epsilon)
+    
+    for i in range(t2.shape[0]):
+        for j in range(t2.shape[1]):
+            posTheta = t2
+            posTheta[i:i+1,j:j+1] = posTheta[i:i+1,j:j+1] + epsilon
+            # Forward propogation algorithm for pos theta
+            z2 = np.append(np.ones((m,1)),X,axis=1).dot(t1.T)
+            a2 = expit(z2)
+            z3 = np.append(np.ones((m,1)),a2,axis=1).dot(posTheta.T)
+            a3 = expit(z3)
+            h = a3
+            posJ = (-1/m)*np.sum( y*np.log(h + 1e-20) + (1-y)*np.log(1-h + 1e-20)) 
+            
+            negTheta = t2
+            negTheta[i:i+1,j:j+1] = negTheta[i:i+1,j:j+1] - epsilon
+    
+    
+            
+            # Forward propogation algorithm for neg theta
+            z2 = np.append(np.ones((m,1)),X,axis=1).dot(t1.T)
+            a2 = expit(z2)
+            z3 = np.append(np.ones((m,1)),a2,axis=1).dot(negTheta.T)
+            a3 = expit(z3)
+            h = a3
+            negJ = (-1/m)*np.sum( y*np.log(h + 1e-20) + (1-y)*np.log(1-h + 1e-20)) 
+            
+            tGrad2[i,j] = (posJ - negJ) / (2 * epsilon)
+    
+    
+    grad = np.concatenate([tGrad1.flat, tGrad2.flat])
+    '''
+
+def prediction(h,y):
+    
+    count =0
+    h_out = np.zeros(h.shape)
+    
+    for i in range(h.shape[0]):
+        if h.shape[1] == 1:
+            if h[i] >= 0.5:
+                h_out[i] = 1
+        else:
+            h_out[i, np.argmax(h[i,:])] =1
+    
+    
+    for i in range(h.shape[0]):
+        for j in range(h.shape[1]):
+            if h_out[i,j]!=y[i,j]:
+                count = count+1
+                break
+    acc = (h.shape[0] - count)/h.shape[0] *100
+    return [h_out, acc]
+
+
+    
+ipSize = x_train.shape[1]
+hlSize = 3 * x_train.shape[1]
+numLabel = y_train.shape[1]
+E0= 0.1
+theta1 = np.random.randint(1,10,(hlSize,ipSize+1))
+theta1 = theta1*2*E0 - E0
+theta2 = np.random.randint(1,10,(numLabel,hlSize+1))
+theta2 = theta2*2*E0 - E0
+initial_theta = np.concatenate([theta1.flat, theta2.flat])
+
+lam =0
+alpha =1
+max_itr = 200
+
+# checking if back propogation is working
+J , grad,h = cost_function_NN1(initial_theta, hlSize, numLabel, x_train, y_train)
+approxGrad = gradient_check(initial_theta, hlSize, numLabel, x_train, y_train)
+
+for i in range(grad.shape[0]):
+  print( "{}      {} " .format(grad[i] , approxGrad[i]))
+
+theta,J_hist = gradient_descent_NN(x_train, y_train, initial_theta, lam, max_itr, alpha, hlSize)
+
+i=np.arange(1,201,1) 
+plt.plot(i,J_hist)
+plt.show()   
+
+
+learning_curve(hlSize,numLabel,x_train, x_val, y_train, y_val, theta, lam)
+
+[J,grad,h_train]=cost_function_NN1(theta, hlSize, numLabel, x_train, y_train)
+h_train,acc=prediction(h_train,y_train)
+print("\naccuracy (train):  ",acc)
+
+[J,grad,h_test]=cost_function_NN1(theta, hlSize, numLabel, x_test, y_test)
+print(h_test)
+h_test,acc=prediction(h_test,y_test)
+
+print("\naccuracy:  ",acc)    
+
+    
+for i in range(y_test.shape[0]):
+    print( "{}      {} " .format(h_test[i] , y_test[i]))
+    
+    
---
+++ neural_iris/data_seperation.py
@@ -0,0 +1,57 @@
+# seperate data for training, validation and testing
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+from numpy import savetxt
+op_cases= 3 
+iris_df = pd.read_csv("Iris.csv",index_col='Id')
+
+def string_to_num(x):
+    if x=='Iris-setosa':
+        return 1
+    if x=='Iris-versicolor':
+        return 2
+    if x=='Iris-virginica':
+        return 3
+
+iris_df['Species']=iris_df['Species'].apply(string_to_num) 
+data=iris_df.to_numpy(dtype='float')
+X=data[:,0:4]
+
+y=np.zeros((X.shape[0],op_cases))
+for i in range(X.shape[0]):
+    if data[i,4:5]==1:
+        y[i,:]= np.identity(3)[0]
+    elif data[i,4:5]==2:
+        y[i,:]= np.identity(3)[1]        
+    elif data[i,4:5]==3:
+        y[i,:]= np.identity(3)[2]
+      
+        
+
+one=np.ones((X.shape[0],1))
+X= np.append(one,X,axis=1)
+
+initial_theta=np.ones((X.shape[1],y.shape[1]))
+
+# make train, val and test sets
+# seperate out test set
+x_1,x_test,y_1,y_test=train_test_split(X,y,test_size=0.2)
+#split remining into train and val set
+x_train,x_val,y_train,y_val=train_test_split(x_1,y_1,test_size=0.2)
+
+Train = np.append(x_train,y_train,axis=1) 
+Val = np.append(x_val,y_val,axis=1)
+Test = np.append(x_test,y_test,axis=1)
+
+
+
+
+columns = "X0,SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm,Sp1,Sp2,Sp3"
+savetxt('TrainData.csv',Train,delimiter=',',header=columns)
+columns = "X0,SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm,Sp1,Sp2,Sp3"
+savetxt('ValData.csv',Val,delimiter=',',header=columns)
+columns = "X0,SepalLengthCm,SepalWidthCm,PetalLengthCm,PetalWidthCm,Sp1,Sp2,Sp3"
+savetxt('TestData.csv',Test,delimiter=',',header=columns)
